e4d95e06ed941a967c5ab53ff4b86e6d1825c906
Thiago Bonfim <bonfimtm@gmail.com>
2019-09-15 10:12:39 +0200

    Add CORS configuration

--- dolphin-service/src/main/java/com/bonfimtm/dolphin/web/ReminderResource.java
+++ dolphin-service/src/main/java/com/bonfimtm/dolphin/web/ReminderResource.java
@@ -6,6 +6,7 @@ import com.bonfimtm.dolphin.domain.Reminder;
 import com.bonfimtm.dolphin.repository.ReminderRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ReminderResource {
 
