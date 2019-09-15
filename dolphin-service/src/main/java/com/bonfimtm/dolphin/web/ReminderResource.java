ea9010ccf3a40fd3eeb86c19481b01f2ab87cd22
Thiago Bonfim <bonfimtm@gmail.com>
2019-09-15 10:26:12 +0200

    Add request mapping to ReminderResource class

--- dolphin-service/src/main/java/com/bonfimtm/dolphin/web/ReminderResource.java
+++ dolphin-service/src/main/java/com/bonfimtm/dolphin/web/ReminderResource.java
@@ -12,31 +12,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
-@RestController
+@RestController()
+@RequestMapping("/reminders")
 public class ReminderResource {
 
     @Autowired
     private ReminderRepository reminderRepository;
 
-    @GetMapping("/reminders")
+    @GetMapping("")
     public Iterable<Reminder> findAll() {
         return reminderRepository.findAll();
     }
 
-    @GetMapping("/reminders/{id}")
+    @GetMapping("/{id}")
     public Optional<Reminder> findById(@PathVariable long id) {
         return reminderRepository.findById(id);
     }
 
-    @PostMapping("/reminders")
+    @PostMapping("")
     public Reminder save(@RequestBody Reminder reminder) {
         return reminderRepository.save(reminder);
     }
 
-    @DeleteMapping("/reminders/{id}")
+    @DeleteMapping("/{id}")
     public void deleteById(@PathVariable long id) {
         reminderRepository.deleteById(id);
     }
