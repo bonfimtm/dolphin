87669713ec7082daa9a5abae17871fd2654f4102
Thiago Bonfim <bonfimtm@gmail.com>
2019-10-19 13:47:09 +0200

    Add production CORS origin

--- dolphin-service/src/main/java/com/bonfimtm/dolphin/web/ReminderResource.java
+++ dolphin-service/src/main/java/com/bonfimtm/dolphin/web/ReminderResource.java
@@ -15,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = { "http://localhost:4200", "https://spinner-dolphin.web.app/",
+        "https://spinner-dolphin.firebaseapp.com", })
 @RestController()
 @RequestMapping("/reminders")
 public class ReminderResource {
